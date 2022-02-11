8939134a59ce6df31c51ade89f27817ab4d38f2c
Ferkelcode <77849645+Ferkelcode@users.noreply.github.com>
2022-02-12 00:08:38 +0100

    Update ContentView.swift
    Update a information block for wall pointer

--- WallAngle2.0/WallAngle/ContentView.swift
+++ WallAngle2.0/WallAngle/ContentView.swift
@@ -26,12 +26,23 @@ struct Marker: Hashable {
     static func markers() -> [Marker] {
         return [
             /*
- 
+            Marker(degrees: 0, label: "Next wall"),
+            
+            Marker(degrees: 30),
+            Marker(degrees: 60),
+            Marker(degrees: 90, label: "E"),
+            Marker(degrees: 120),
+            Marker(degrees: 150),
+            Marker(degrees: 180, label: "S"),
+            Marker(degrees: 210),
+            Marker(degrees: 240),
+            Marker(degrees: 270, label: "W"),
+            Marker(degrees: 300),
+            Marker(degrees: 330)
             */
         ]
     }
 }
-
 struct CompassMarkerView: View {
     let marker: Marker
     let compassDegress: Double
@@ -41,6 +52,7 @@ struct CompassMarkerView: View {
             Text(marker.degreeText())
                 .fontWeight(.light)
                 .rotationEffect(self.textAngle())
+                .border(Color.purple)
             
             Capsule()
                 .frame(width: self.capsuleWidth(),
@@ -69,7 +81,7 @@ struct CompassMarkerView: View {
     private func textAngle() -> Angle {
         return Angle(degrees: -self.compassDegress - self.marker.degrees)
     }
-     
+   
 }
 
 struct DeviceRotationViewModifier: ViewModifier {
@@ -108,19 +120,81 @@ struct ContentView: View {
     
     var body: some View {
         //To make sure the phone is hold horitontally.
-        Group {
-                    if orientation.isPortrait {
-                        Text("Please hold your phone horizontally.").foregroundColor(.red).fontWeight(.bold)
-                    } else if orientation.isLandscape {
-                        Text("Please hold your phone horizontally.").foregroundColor(.red).fontWeight(.bold)
-                    } else if orientation.isFlat {
-                        Text(" ").foregroundColor(.green)
-                    } else {
-                        Text(" ").foregroundColor(.red).fontWeight(.bold)
-                    }
-        }
+        /*
         VStack {
+            Text("This text is outside of the top safe area.")
+                .edgesIgnoringSafeArea([.top])
+                .border(Color.purple)
+                //.position(x: 20, y: 0)
+            Spacer()
+
+        }
+        .frame(width: 200, height: 30)
+        .border(Color.gray)
+        //.position(x: 50, y: 0)
+        .edgesIgnoringSafeArea([.top])
+        
+        Text("This text is outside of the top safe area.")
+             .edgesIgnoringSafeArea([.top])
+             .border(Color.purple)
+        */
+        
+            
+            
+
+            
+        VStack {
+            Group{
+                //Spacer()
+                if (orientation.isFlat && !gotsecond){
+                    VStack{
+                        Text("                                                                                       ")
+                        .edgesIgnoringSafeArea([.top])
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height:36)
+                        HStack{
+                            Image("arrow").frame( height: 20)
+                            Text("Wall").frame( height: 20)
+                        }
+                        
+                        
+                    }
+                    Spacer()
+                    
+                }else {
+                    Text("else")
+                    .foregroundColor(.red)
+                    .edgesIgnoringSafeArea([.top])
+                    //.border(Color.purple)
+                    .frame(height: 100)
+                    //.position(x: 20, y: 0)
+                Spacer()
+                }
+            }
+            
+            Group {
+               
+                        if orientation.isPortrait {
+                            Text("Please hold your phone horizontally.").foregroundColor(.red).fontWeight(.bold).frame(width: 400, height: 70).font(.system(size: 22))
+                        } else if orientation.isLandscape {
+                            Text("Please hold your phone horizontally.").foregroundColor(.red).fontWeight(.bold)
+                        } else if orientation.isFlat {
+                            Text(" ").foregroundColor(.green).frame(width: 400, height: 30)
+                        } else {
+                            Text(" ").foregroundColor(.red).fontWeight(.bold).frame(width: 400, height: 54)
+                        }
+            }
+            
             if self.orientation.isFlat{ //only when the orientation is Flat
+               
+                    //.fill(Color.blue)
+                
+
+                
+                //for the button
                 Button(self.buttonText) {
                     if(!gotfirst){
                         Wall1 = self.compassHeading.degrees
@@ -146,9 +220,19 @@ struct ContentView: View {
                 .background(Color.blue)
                 .cornerRadius(40)
                 .foregroundColor(.white)
+                
+                //.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+                //.border(Color.red)
+                //.frame(maxWidth: .infinity, // Full Screen Width
+                           // maxHeight: .infinity, // Full Screen Height
+                          //  alignment: .topLeading)
+                //.frame(maxWidth: .infinity, maxHeight: .infinity)
+                //for the button
+                
+
             }
             
-            
+            //for the 2 marker lines
             ZStack {
                            ForEach(Marker.markers(), id: \.self) { marker in
                                CompassMarkerView(marker: marker,
@@ -158,7 +242,7 @@ struct ContentView: View {
                            }
                 
                 //*******should be after button click*********
-                //turn the second line
+                //for the second line
                 if (gotfirst && !gotsecond){
                     Capsule()
                         .frame(width: 5, height: 70)
@@ -171,6 +255,7 @@ struct ContentView: View {
                         .frame(width: 5, height: 70)
                         .foregroundColor(.blue)
                         .rotationEffect(SwiftUI.Angle(degrees: Wall2-Wall1), anchor: .bottom)
+                        
                 }
                 //********************+
                 
@@ -180,20 +265,27 @@ struct ContentView: View {
                     .frame(width: 5, height: 70)
                     .foregroundColor(.orange)
                     //.fill(Color.blue)
+
                     
             }
             .frame(width: 300, height: 200)
             //.rotationEffect(SwiftUI.Angle(degrees: self.compassHeading.degrees))
             .statusBar(hidden: true)
             
+            
+            //.position(x: 200, y: 80)
+            
+            //for the second line
+            
             if gotsecond {
                 Text("The inner angle is: \(String(format: "%.2f", Angle))")
-                    .font(.system(size: 30))
+                    .font(.system(size: 27))
                 Text("The outer angle is: \(String(format: "%.2f", outerAngle))")
-                    .font(.system(size: 30))
-                
+                    .font(.system(size: 27))
+                //Spacer()
             }
-            
+            Spacer()
+                
         }
         .onRotate { newOrientation in
             orientation = newOrientation
@@ -201,5 +293,7 @@ struct ContentView: View {
         .alert(isPresented: $showAlert) {
             Alert(title: Text("Introduction"), message: Text("To calibrate: Hold your phone up and then tilt it horizontally. Keep it horizontally during the measurement"), dismissButton: .default(Text("Got it!")))
         }
+        .edgesIgnoringSafeArea([.top])
+        //Spacer()
    }
 }
